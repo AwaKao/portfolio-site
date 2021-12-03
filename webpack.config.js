a3e28073520e3a83ca484ac1bd8f9dfdd820dc09
AwaKao <saitoh.kao@gmail.com>
2021-12-03 19:40:59 +0900

    環境構築

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name]-[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { 'targets': '> 0.25%, not dead' }
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)/,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70,
+              }
+            },
+          }
+        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name]-[contenthash][ext]',
+        }
+      },
+      {
+        test: /\.(woff|woff2)/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/[name]-[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new CleanWebpackPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: './src/favicon.svg',
+      prefix: '',
+      mode: 'light',
+    }),
+  ],
+}
