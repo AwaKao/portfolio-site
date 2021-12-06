6c253dcf4d36ceacdd2b53253978223c21eee216
AwaKao <saitoh.kao@gmail.com>
2021-12-06 15:04:57 +0900

    PCのtopからnaviのアニメーション追加

--- src/js/function.js
+++ src/js/function.js
@@ -1,5 +1,5 @@
 $(function() {
-  // loading=>top
+  // loading=>top animation
   window.onload = function() {
     setTimeout(function(){
       // PC
@@ -9,4 +9,13 @@ $(function() {
       $('#contact_pc').removeClass('pc-navi__contact-navi_loading').addClass('pc-navi__contact-navi_top');
     },1500);
   }
+
+  // top=>navi animation
+  $('#about_pc, #works_pc, #contact_pc').click(function() {
+    // PC
+    $('#ka_pc').removeClass('pc-navi__ka-navi_top').addClass('pc-navi__ka-navi_contents');
+    $('#about_pc').removeClass('pc-navi__about-navi_top').addClass('pc-navi__about-navi_contents');
+    $('#works_pc').removeClass('pc-navi__works-navi_top').addClass('pc-navi__works-navi_contents');
+    $('#contact_pc').removeClass('pc-navi__contact-navi_top').addClass('pc-navi__contact-navi_contents');
+  });
 });
